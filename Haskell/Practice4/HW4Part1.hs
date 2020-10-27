092fdebd1bcb24efd94443ae18a95deab84c3e6e
Glee <gl2429@columbia.edu>
2020-10-26 23:47:41 -0700

    Update HW4Part1.hs

--- Haskell/Practice4/HW4Part1.hs
+++ Haskell/Practice4/HW4Part1.hs
@@ -1,3 +1,47 @@
+{- HW4 
+
+Consider the following abstract syntax, which describes a language that manipulates three integer registers named A, B, and R.
+
+int	::=	(any integer)	integers
+ 			
+reg	::=	A  |  B  |  R	register names
+ 			
+expr	::=	int	           -- integer literal
+       |	   reg	           -- load from register
+       |	   expr + expr	     -- integer addition
+       |	   expr <= expr	  -- less than or equal to
+       |	   not expr	        -- boolean negation
+ 			
+stmt	::=	reg := expr	      -- store to register
+       |	   if expr	         -- conditional statement
+            then prog	
+            else prog	
+            end	
+       |	   do prog	         -- loop until break
+            end	
+       |	   break	            -- break out of a loop
+ 			
+prog	::=	ε  |  stmt ; prog	-- sequence of statements
+
+
+Note that although the expression language may produce both booleans and integers, the registers may only contain integers.
+
+Here is an example of a short program in this language that multiplies the numbers in A and B, storing the result in R. Comments, which are not reflected in the abstract syntax, are included in curly braces { … }.
+
+A := 7;           { initialize the registers }
+B := 9;
+R := 0;
+do
+  if A <= 0 then  { loop until A is 0 }
+    break;
+  else
+    R := R + B;   { ... add B to R }
+    A := A + -1;  { ... decrement A }
+  end;
+end;
+
+-}
+
 {- Part I -}
 
 -- Question 1. Encode the abstract syntax for the language as a set of Haskell types and data types.
