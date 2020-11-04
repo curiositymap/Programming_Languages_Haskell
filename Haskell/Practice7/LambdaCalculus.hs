b8d28d1a87ec8dff58901c61c3e03777dce27c7f
Ga Young Lee <leegay@oregonstate.edu>
2020-11-03 20:12:51 -0800

    Lambda Calculus

---
+++ Haskell/Practice7/LambdaCalculus.hs
@@ -0,0 +1,34 @@
+module LambdaCalculus where
+
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+-- * Syntax
+
+type Var = String
+
+data Exp
+   = Ref Var
+   | App Exp Exp
+   | Abs Var Exp
+   deriving(Eq,Ord,Show)
+
+
+-- * Syntactic sugar
+
+-- | Define a binary function
+
+abs2 :: Var -> Var -> Exp -> Exp
+abs2 x y e = Abs x (Abs y e)
+
+-- | Define a ternary function
+abs3 :: Var -> Var -> Var -> Exp -> Exp
+abs3 x y z e = Abs x (Abs y (Abs z e))
+
+-- | Apply a binary function
+app2 :: Exp -> Exp -> Exp -> Exp
+app2 f e1 e2 = App (App f e1) e2
+
+-- | Apply a ternary function
+app3 :: Exp -> Exp -> Exp -> Exp -> Exp
+app3 f e1 e2 e3 = App (App (App f e1) e2) e3
