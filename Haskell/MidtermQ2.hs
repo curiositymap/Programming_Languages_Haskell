5acb0d6a526484bbd3a98ed2f7a27825211d86ce
Ga Young Lee <leegay@oregonstate.edu>
2020-11-06 21:51:00 -0800

    midterm solution

--- Haskell/MidtermQ2.hs
+++ Haskell/MidtermQ2.hs
@@ -1,7 +1,33 @@
+{- Grammar --> syntax -}
 
+{-
+Given the grammar below, define a syntax.
+
+i ∈ Int   ::=   (any integer)
+v ∈ Var   ::=   (any variable name)
+e ∈ Exp   ::=   i   |   v   |   e + e   |   e - e
+s ∈ Stmt   ::=   v ← e   |   while e ≤ e do s   |   begin s* end
+
+Given a programming below, create an AST using Haskell Type and Data Types.
+
+begin
+    quot ← 0
+    rem ← 53
+    while 8 ≤ rem do begin
+        quot ← quot + 1
+        rem ← rem - 8
+    end
+end
+
+ 
+-}
+
+-- Caveats : Var can be any "STRING"
+-- When you're defining the AST of the given program,
+-- Make sure to use "STRING" to denote STRING!!!
 
 data Var
-   = VarLit Char
+   = VarLit String
 
 data Exp
    = Lit Int
@@ -17,12 +43,14 @@ data Stmt
 example :: Stmt
 
 example =
-  BEnd [ Set (VarLit quot) (Lit 0)
-       , Set (VarLit rem) (Lit 53)
-       , While
-        (Lit 8) (Get rem)
-        BEnd [
-          Set (VarLit quot) (Add (Get quot) (Lit 1))
-        , Set (VarLit rem) (Sub (Get rem) (Lit 1))
+  BEnd
+  [ Set (VarLit "quot") (Lit 0)
+  , Set (VarLit "rem") (Lit 53)
+  , While
+        (Lit 8) (Get (VarLit "rem"))
+        BEnd
+        [
+          Set (VarLit "quot") (Add (Get (VarLit "quot")) (Lit 1))
+        , Set (VarLit "rem") (Sub (Get (VarLit "rem")) (Lit 8))
         ]
        ]
